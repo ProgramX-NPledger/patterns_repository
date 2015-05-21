1dc71ab8118ed748d228fd134099870fa3267469
Nathan Pledger <nathan@programx.co.uk>
2015-05-21 20:02:17 +0100

    Initial project structure

---
+++ Patterns.Repository.Data/sql/0022 Invoice.sql
@@ -0,0 +1,7 @@
+﻿SET IDENTITY_INSERT [dbo].[Invoice] ON
+INSERT INTO [dbo].[Invoice] ([Id], [CustomerID], [InvoiceDate], [TermsDays]) VALUES (1, 1, N'2010-01-01 00:00:00', 30)
+INSERT INTO [dbo].[Invoice] ([Id], [CustomerID], [InvoiceDate], [TermsDays]) VALUES (2, 2, N'2013-01-02 00:00:00', 90)
+INSERT INTO [dbo].[Invoice] ([Id], [CustomerID], [InvoiceDate], [TermsDays]) VALUES (3, 3, N'2010-01-01 00:00:00', 30)
+INSERT INTO [dbo].[Invoice] ([Id], [CustomerID], [InvoiceDate], [TermsDays]) VALUES (4, 1, N'2010-01-01 00:00:00', 30)
+INSERT INTO [dbo].[Invoice] ([Id], [CustomerID], [InvoiceDate], [TermsDays]) VALUES (5, 4, N'2012-02-02 00:00:00', 30)
+SET IDENTITY_INSERT [dbo].[Invoice] OFF
